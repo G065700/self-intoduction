36da4077e0808e685bdf5037cbe6feff28adb5c1
G065700 <g065700@naver.com>
2021-06-14 17:59:39 +0900

    주석 추가 및 main.css 정리

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
+// MAIN 영역 class="intro"인 요소에 텍스트 및 타이핑 효과 추가
 const introContent = '프론트엔드 개발자, 고은별의 웹 포트폴리오 입니다.'
-// console.log(introContent[0])
 const introEl = document.querySelector(".intro")
 
 let index = 0
 setInterval(() => {
   introEl.textContent += introContent[index]
-  // console.log('index', index)
-  // console.log(introEl.textContent)
   ++index
-  // console.log('++index', index)
-  // console.log(introContent.length)
   if (index > introContent.length) {
     introEl.textContent = ""
     index = 0
   }
 }, 200)
 
+// HEADER 영역 class="header"인 요소에 position="fixed"를 위한 클래스 값 추가
 const headerEl = document.querySelector(".header")
-// console.log(window.pageYOffset, headerEl.offsetTop)
 
 window.addEventListener('scroll', () => {
   if (visualViewport.height < window.scrollY) {
@@ -27,6 +23,7 @@ window.addEventListener('scroll', () => {
   }
 })
 
+// HEADER 영역의 각 메인 메뉴를 클릭하면 해당 페이지로 스크롤되는 효과 추가
 const menuEls = document.querySelectorAll('.menu li')
 const headerElHeight = headerEl.offsetHeight
 
@@ -60,16 +57,13 @@ menuEls[3].addEventListener('click', () => {
 })
 menuEls[4].addEventListener('click', () => {
   window.scrollTo({
-    top: document.querySelector('.skill').offsetTop,
+    top: document.querySelector('.skills').offsetTop,
     left: 0,
     behavior: 'smooth'
   })
-});
-// const divEl = document.createElement('div')
-// divEl.setAttribute("class", "line")
-// console.log(divEl)
-// const careerContentEl = document.querySelector('.career .content')
+})
 
+// PROJECT 영역에 Swiper 효과 추가
 new Swiper('.project .swiper-container', {
   direction: 'horizontal',
   loop: true,
@@ -88,18 +82,6 @@ new Swiper('.project .swiper-container', {
   }
 });
 
-
-
-// window.addEventListener('scroll', () => {
-//   if (window.pageYOffset >= headerEl.offsetTop) {
-//     headerEl.classList.add("fixed")
-//   } else {
-//     headerEl.classList.remove("fixed")
-//   }
-// })
-/**
- * 올해가 몇 년도인지 계산
- */
+// FOOTER 영역 내 class="this-year" 요소에 올해가 몇년도 인지 계산하여 노출
 const thisYear = document.querySelector('.this-year')
 thisYear.textContent = new Date().getFullYear()
-
